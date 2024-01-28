893e65b05fec49bde6579b3ce18bad0dccc61036
Kirill <pavlushevsky@gmail.com>
2024-01-28 11:39:32 +0300

    now commit

--- in.php
+++ in.php
@@ -1,4 +1,64 @@
 <?php
-    echo $_POST['name'];
+    $idnow;
+    try{
+        $pdo = new PDO('mysql:host=localhost;dbname=test;','root','');
+    }
+    catch(PDOException $e){
+        echo "Подключение к бд отсутствует";
+    }
+    if($_POST['id'] == '1'){
+        $query = "SELECT * FROM `table1` WHERE `email` = '".$_POST['email']."'";
+        $a = 0; 
+        foreach($pdo -> query($query) as $row){
+            $a++;
+        }
+        if($a == 0){
+            $sql = "INSERT INTO `table1`(`name`, `email`, `town`, `password`) VALUES ('".$_POST['name']."','".$_POST['email']."','".$_POST['town']."','".$_POST['password']."');";
+            $result = $pdo->query($sql);
+            $query1 = "SELECT * FROM `table1` WHERE `email` = '".$_POST['email']."' AND `name` = '".$_POST['name']."' AND `password` = '".$_POST['password']."'";
+            $a1 = 0; 
+            foreach($pdo -> query($query1) as $row1){
+                $a1++;
+                $idnow = $row1['id'];
+            }
+            $query4 = "SELECT * FROM `table1` WHERE `id`=$idnow";
+            echo '<table>';
+            foreach($pdo -> query($query4) as $row4){
+                echo "<tr>";
+                echo "<td>'".$row4['name']."'</td>";
+                echo "<td>'".$row4['email']."'</td>";
+                echo "<td>'".$row4['town']."'</td>";
+                echo "<td>'".$row4['password']."'</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        }
+        else{
+            echo "Такая почта уже существует";
+        }
+    }
+    if($_POST['id'] == '2'){
+        $query2 = "SELECT * FROM `table1` WHERE `email` = '".$_POST['email']."' AND `name` = '".$_POST['name']."' AND `password` = '".$_POST['password']."'";
+        $a2 = 0; 
+        foreach($pdo -> query($query2) as $row2){
+            $a2++;
+            $idnow = $row2['id'];
+        }
+        if(a == 0){
+            echo "Неправильные данные";
+        }
+        else{
+            $query3 = "SELECT * FROM `table1` WHERE `id`=$idnow";
+            echo "<table>";
+            foreach($pdo -> query($query3) as $row3){
+                echo "<tr>";
+                echo "<td>'".$row4['name']."'</td>";
+                echo "<td>'".$row4['email']."'</td>";
+                echo "<td>'".$row4['town']."'</td>";
+                echo "<td>'".$row4['password']."'</td>";
+                echo "</tr>"; 
+                echo "</table>";
+            }
+        }
+    }
 ?>
-<!-- INSERT INTO `table1`(`id`, `name`, `email`, `town`, `gender`, `password`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]') -->
--- script.js
+++ script.js
@@ -1,8 +1,100 @@
-$(window).ready(function(){
-    $('#submit').click(function(){
-        $.post('in.php',{name : $('#name').val() 
-        },function(data){
+$(document).ready(function () {
+    let a = 0;
+    // переключение форм
+    $('#change').click(function () {
+        if (a == 0) {
+            a++;
+            $('#firstForm').fadeToggle(1000);
+            function secondForm() {
+                $('#secondForm').fadeToggle(1000);
+            }
+            setTimeout(secondForm, 999);
+        }
+        else {
+            a--;
+            $('#secondForm').fadeToggle(1000);
+            function firstForm() {
+                $('#firstForm').fadeToggle(1000);
+            }
+            setTimeout(firstForm, 999);
+        }
+    });
+    //Проверка на (какая формасейчас активна)
+    if(a == 0){
+        // активация проверки и действий при нажатии на кнопку (формы регестрации)
+        $('#submit').click(function () {
+            // проверка на пустые поля
+            if ($('#name').val() != 0 && $('#email').val() != 0 && $('#town').val() != 0 && $('#password').val() != 0 && $('#password2').val() != 0 && $('#password').val() == $('#password2').val()) {
+                //связь с in.php
+                $.post('in.php', {
+                    id: '1',
+                    name: $('#name').val(),
+                    email: $('#email').val(),
+                    town: $('#town').val(),
+                    password: $('#password').val()
+                }, function (data) {
+                    if(data != ''){
+                        alert(data);
+                    }
+                    else{
+                        alert(data);
+                        success();
+                    }
+                });
+            }
+            else {
+                //возврат результата при нулевом значении какого-либо input
+                if ($('#name').val() == 0) {
+                    alert("Пустое значение имени");
+                }
+                else if ($('#email').val() == 0) {
+                    alert("Пустое значение почты");
+                }
+                else if ($('#town').val() == 0) {
+                    alert("Не выбран город!");
+                }
+                else if ($('#password').val() == 0) {
+                    alert("Значение пароля пустое!");
+                }
+                else if ($('#password2').val() == 0) {
+                    alert("Значение подтверждения пароля пустое!");
+                }
+                else if ($('#password').val() != $('#password2').val()) {
+                    alert("Пароль и подтверждающий пароль не совпадают!");
+                }
+                else {
+                    alert("Ошибка!");
+                }
+            }
+        });
+    }
+    else{
+        // активация события (проверка на овпадение имени, пароля и почты).
+        $('#submit2').click(function (){
+            $.post('in.php', {
+                id: '2',
+                name: $('#name2').val(),
+                email: $('#email2').val(),
+                password: $('#password12').val()
+            }, function (data) {
+                if(data != ''){
+                    alert(data);
+                }
+                else{
+                    alert(data);
+                    success();
+                }
+            });
+        });
+    }
+    // функция активации таблицы и дизактивации форм
+    function success(){
+        a = 2;
+        $('firstForm').fadeHide(1000);
+        $('secondForm').fadeHide(1000);
+        $.post('in.php',{id: 3},function(data){
+            $(document).innerHTML(data);
             alert(data);
         });
-    });
+    }
 });
